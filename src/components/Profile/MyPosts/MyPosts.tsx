675cde58c61a1e395310db18f1ee03dc91ad51e1
nikolasmushroom <dashagribok0905@gmail.com>
2024-08-22 23:57:55 +0300

    Adding map-method in MyPost.tsx

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,22 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import s from '../MyPosts/MyPosts.module.css'
-import {Post} from "./Post/Post";
+import {Post, PostPropsType} from "./Post/Post";
 
 export const MyPosts = () => {
+    const [currentPosts, setCurrentPosts] = useState<Array<PostPropsType>>([
+        {message: 'hello world'},
+        {message: 'Hello Dasha'},
+        {message: 'Privet Andrey'}
+    ])
     return (
         <div>
             <div>
                 My posts
             </div>
-
+            <input/><button>Add new post</button>
             <div>
                 New posts
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                {currentPosts.map((post) => <Post message={post.message}/>)}
             </div>
         </div>
 )
