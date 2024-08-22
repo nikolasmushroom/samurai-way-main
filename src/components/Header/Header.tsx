8bef4b905257f2ad55eb0132e92ddd8649a98e50
nikolasmushroom <dashagribok0905@gmail.com>
2024-08-22 23:26:52 +0300

    Adding logo and justifing content

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import classes from './Header.module.css';
 const Header = () => {
     return (
         <header className={classes.header}>
-            <img src='' alt="logo"/>
+            <img src='https://cdn.worldvectorlogo.com/logos/blue-1.svg' alt="logo"/>
         </header>
     )
 }
