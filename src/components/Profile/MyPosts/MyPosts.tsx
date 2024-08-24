81d65a4f2a596136143709413d61f4ea74161e94
nikolasmushroom <dashagribok0905@gmail.com>
2024-08-24 23:36:58 +0300

    Adding clean of input after pushing button and adding function onKeyDownHandler

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,17 +1,28 @@
-import React, {useState} from "react";
+import React, {KeyboardEvent, useState} from "react";
 import s from '../MyPosts/MyPosts.module.css'
 import {Post, PostPropsType} from "./Post/Post";
+import classes from "./MyPosts.module.css";
 
 export const MyPosts = () => {
     const [currentPosts, setCurrentPosts] = useState<Array<PostPropsType>>([])
     const [currentInput, setCurrentInput] = useState('')
+    const addNewPost = () => {setCurrentPosts([...currentPosts, { message: currentInput }]); setCurrentInput('')};
+    const onKeyDownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === 'Enter'){
+            setCurrentPosts([...currentPosts, { message: currentInput }])
+            setCurrentInput('');
+        }
+    }
     return (
         <div>
             <div>
                 My posts
             </div>
-            <input value={currentInput} onChange={(e) => (setCurrentInput(e.currentTarget.value))}/>
-            <button onClick={() => setCurrentPosts([...currentPosts, { message: currentInput }])}>Add new post</button>
+            <input className={classes.input} value={currentInput}
+                   onChange={(e) => (setCurrentInput(e.currentTarget.value))}
+                   onKeyDown={onKeyDownHandler}
+            />
+            <button onClick={addNewPost}>Add new post</button>
             <div>
                 New posts
             </div>
